e3e4dea1e0b58191c82fd46b2b5c7bc31fb79493
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-02 22:08:47 +0000

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
 """Generates the pascal triangle up to n rows
 """
-# def pascal_triangle(n):
-#     if n <= 0:
-#         return []
-#     if n == 1:
-#         return [[1]]
-#     else:
-#         new_row = [1]
-#         result = pascal_triangle(n-1)
-#         last_row = result[-1]
-#         for i in range(1, len(last_row)):
-#             new_row.append(last_row[i-1] + last_row[i])
-#         new_row.append(1)
-#         result.append(new_row)
-#     return result
-    
 def pascal_triangle(n):
     """Generates the pascal triangle up to n rows
 
