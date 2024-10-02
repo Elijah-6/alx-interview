ad63bb4d0ea19776912b608bd7c40116826433e5
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-02 21:51:14 +0000

    use recursion

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
-    """Generates the pascal triangle up to n rows
-
-    Args:
-        n (int): number of rows
-
-    Returns:
-        array: pascal triangle
-    """
     if n <= 0:
         return []
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-    return triangle
+    if n == 1:
+        return [[1]]
+    else:
+        new_row = [1]
+        result = pascal_triangle(n-1)
+        last_row = result[-1]
+        for i in range(1, len(last_row)):
+            new_row.append(last_row[i-1] + last_row[i])
+        new_row.append(1)
+        result.append(new_row)
+    return result
+    
+
+# def pascal_triangle(n):
+#     """Generates the pascal triangle up to n rows
+
+#     Args:
+#         n (int): number of rows
+
+#     Returns:
+#         array: pascal triangle
+#     """
+#     if n <= 0:
+#         return []
+#     triangle = [[1]]
+#     for i in range(1, n):
+#         row = [1]
+#         for j in range(1, i):
+#             row.append(triangle[i-1][j-1] + triangle[i-1][j])
+#         row.append(1)
+#         triangle.append(row)
+#     return triangle
