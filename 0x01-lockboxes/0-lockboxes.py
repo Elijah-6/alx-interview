d258ce0cae215227c4696b69b2a19fa35230b008
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-11 15:22:40 +0000

    Fix: pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,7 +7,25 @@ Functions:
     canUnlockAll(boxes): Determines if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be unlocked.
+
+    A list of lists is given where each inner list
+    represents a box and contains keys to other boxes.
+    The function returns True if all boxes can be
+    unlocked starting from the first box (index 0),
+    otherwise it returns False.
+
+    Parameters:
+    boxes (list of list of int): A list of lists
+    where each inner list contains integers representing keys.
+
+    Returns:
+    bool: True if all boxes can be unlocked,
+    False otherwise.
+    """
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
