3050c670c7e1dbd8f6afbc68f52e1770ecbf7b43
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-31 14:33:52 +0000

    Refactor UTF-8 validation module

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+"""UTF-8 validation module.
+"""
+
+
 def validUTF8(data):
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
+    """
     # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
