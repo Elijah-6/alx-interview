5e85d70c8e37040ef0c49136fe8fc0ea8def2cf5
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-12-07 11:12:13 +0000

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 """Island perimeter computing module.
 """
+
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of an island represented by 1s in a grid of 0s and 1s.
-    Args:
-        grid (List[List[int]]): A 2D list representing the grid where 1s are land and 0s are water.
-    Returns:
-        int: The perimeter of the island.
-    The function iterates through each cell in the grid. For each land cell (1), it checks its four neighbors
-    (top, bottom, left, right). If a neighbor is water (0) or the cell is on the boundary of the grid, it 
-    contributes to the perimeter.
-    """
+
     if type(grid) != list:
         return 0;
     
